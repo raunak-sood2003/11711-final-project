2e0ab357cc7acc05f4519972c3f0bdc3171d31cc
eric102004 <h511163@nehs.hc.edu.tw>
2024-11-07 14:34:04 -0500

    fix: fix bugs in qwen_humaneval.py

--- qwen_humaneval.py
+++ qwen_humaneval.py
@@ -17,7 +17,7 @@ prompt_template = """
 def write_json_file(dicts, output_file):
     with open(output_file, 'w') as f:
         for d in dicts:
-            f.write(json.dumps(d)i+'\n')
+            f.write(json.dumps(d)+'\n')
 
 def process_model_input(prompt):
     messages = [
@@ -47,14 +47,15 @@ dataset = load_dataset("openai_humaneval")
 
 
 outputs = []
-for data in tqdm(dataset['test']):
+for i, data in enumerate(dataset['test']):
+    print(f"{i} / {len(dataset['test'])}")
     # parse data
     task_id = data['task_id']
     prompt = data['prompt']
     
 
     # repeat 100 times
-    for r in range(repeat):
+    for r in tqdm(repeat):
         # build model input
         prompt = prompt_template.format(code_prompt=prompt)
         model_inputs = process_model_input(prompt)
@@ -70,14 +71,14 @@ for data in tqdm(dataset['test']):
         )
         generated_ids = generation_output.sequences
         logprobs = model.compute_transition_scores(generation_output.sequences, generation_output.scores, normalize_logits=True)
-        logprobs = logprobs[0].detach().cpu().to_list()
+        logprobs = logprobs[0].detach().cpu().tolist()
         #logits = generation_output.sequences.logits
         #logprobs = F.log_softmax(logits, dim=-1).detach().cpu().to_list()
 
         response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True, output_logits=True)[0]
 
         # record output
-        output_dict = {"task_id": task_id, \ 
+        output_dict = {"task_id": task_id, \
                         "completion": response, \
                         "logprobs": logprobs, \
                         }
